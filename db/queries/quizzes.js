f13321b5a7acf8169870b6da3bc4fe56a656902a
workuseifu1 <workuseifu1@gmail.com>
2023-07-23 17:12:27 -0500

    update quizzes.js

--- db/queries/quizzes.js
+++ db/queries/quizzes.js
@@ -18,8 +18,30 @@ const getQuizzesByUserId = (id) => {
       return res.rows;
     });
 };
+ //all questions by quizId
+ const getQuestionsByQuizId = (id) => {
+  return db
+    .query(`SELECT * FROM questions WHERE quiz_id IN ($1)`, [id])
+    .then((res) => {
+      return res.rows;
+    });
+ }
+
+ //submitQuiz takes user_id, name, is_private and private_id
+ const submitQuiz = (quizObj) => {
+  const {user_id, quiz_name, is_private, private_id} = quizObj; //destructure
+  const queryText = `INSERT INTO quizzes(user_id, name, is_private, private_id) VALUES ($1, $2, $3, $4) RETURNING id`;//insert new row to 'quizzes' table
+  const values = [user_id, quiz_name, !!private, private_id];//Values to be inserted to db
+
+  return (
+    db
+      .query(queryText,values) //query pass to cb function
+      .then((res) =>)
+  );
+ }
 
 module.exports = {
   getPublicQuizzes,
-  getQuizzesByUserId,  
+  getQuizzesByUserId, 
+  getQuestionsByQuizId 
 };
