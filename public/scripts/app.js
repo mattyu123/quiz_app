84177051f7c463b5c2c468ad7e5204eb5f1d1a5b
workuseifu1 <workuseifu1@gmail.com>
2023-07-23 16:13:16 -0500

    work on private quiz

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,9 @@
 // Client facing scripts here
+$(document).ready(function(){
+  $("show_url").hide();
+
+  $("#share").on("click",function(event) {
+    event.preventDefault();
+    $("#show_url").show();
+  });
+});
