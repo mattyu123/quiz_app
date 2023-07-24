4703ab456dfd6d369c7b3ba8cd039dee761b3f7a
mattyu123 <matthewyu100@gmail.com>
2023-07-24 17:19:49 -0400

    update database with private quiz

---
+++ db/seeds/02_seeds.sql
@@ -0,0 +1,7 @@
+--Populate initial private quiz
+INSERT INTO quizzes (user_id, name, is_private, url)
+VALUES (1, 'Video Games', true, 'placeholder url');
+
+INSERT INTO questions (quiz_id, question, option_1, option_2, option_3, option_4)
+VALUES (2, 'What is the name of the Italian plumber whose favourite mode of transportation is by pipe and loves to eat mushrooms?', 'Mario','Guiseppe','Tony','Felipe'),
+(2, 'What is the best selling video game console of all time?','Playstation 2','Nintendo DS','XBOX 360','Playstation 4');
